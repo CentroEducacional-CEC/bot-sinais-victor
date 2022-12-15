af7edbb68d2962c61699f542258d354ad5761193
Neto <adrianoneto2501@gmail.com>
2022-12-15 03:20:46 -0300

    change to offline period

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -2,6 +2,7 @@ const Telegraf = require('telegraf');
 require('dotenv').config();
 const token = process.env.token;
 const bot = new Telegraf(token);
+// bot.telegram.deleteWebhook();
 const moment = require('moment');
 const momentTimeZone = require('moment-timezone');
 
@@ -29,6 +30,7 @@ const maxRangeMinute = 6;
 
 bot.start(async (ctx, next) => {
     ctx.reply(`🟢Bot online! Próximo sinal em aproximadamente 3 a 5 minutos\n\n ⚠OBS: Caso estejamos entre 3h da manhã e 8h da manhã o bot só enviará os primeiros sinais a partir das 8h da manhã\n\n⏸Para pausar o bot, digite: /stopbet`);
+
     pauseBot = false;
     
     function foo_interval_action() {
@@ -44,11 +46,11 @@ bot.start(async (ctx, next) => {
             console.log('-----------')
             console.log('OFFline period = ' + offlinePeriod);
                 
-                if (RedOrGreen < 0.7 && onlinePeriod) {
+                if (RedOrGreen < 0.7 && !onlinePeriod) {
                     bot.telegram.sendMessage(process.env.TELEGRAM_CHANNEL, '🟣 APOSTE AGORA 🟣\n\n🚀 Saque Aut. em 1,5x / 2x*\n(50% Saque Aut. em 1,3x)\n\n🔄 Fazer no máx. G1\n\n(Recuperar dobrando a aposta)\n\n⏰ Entrar 10/15 segundos antes/depois\n\n(Analisar rodadas anteriores)\n⚠️ Gerenciamento de banca\n\n(Se perder a culpa não é minha!)');
                     setTimeout(foo_interval_action,  getRandomInterval(minimumRangeMinute, maxRangeMinute));
                     console.log('GREEN aposte agora!' + timezoneRecifeFormated());
-                } else if (RedOrGreen >= 0.7 && onlinePeriod) {
+                } else if (RedOrGreen >= 0.7 && !onlinePeriod) {
                     bot.telegram.sendMessage(process.env.TELEGRAM_CHANNEL, '🟣 APOSTE AGORA 🟣\n\n🚀 Saque Aut. em 1,5x / 2x*\n(50% Saque Aut. em 1,3x)\n\n🔄 Fazer no máx. G1\n\n(Recuperar dobrando a aposta)\n\n⏰ Entrar 10/15 segundos antes/depois\n\n(Analisar rodadas anteriores)\n⚠️ Gerenciamento de banca\n\n(Se perder a culpa não é minha!)');
                     setTimeout(foo_interval_action,  getRandomInterval(minimumRangeMinute, maxRangeMinute));
                     console.log(('RED CUIDADO!') + timezoneRecifeFormated());
@@ -80,7 +82,7 @@ bot.startPolling();
 function getRandomInterval(min, max) {
     let randomInterval = Math.floor(Math.random() * (max - min) + min); // Not include the max value
     console.log('randomInterval = Daqui a ' + randomInterval + ' minutos. TimeZone Recife AGORA = ' + timezoneRecifeFormated());
-    return randomInterval * 60000;
+    return randomInterval * 1000;
 };
 
 // console.log(typeof defaultTimeZone + ' Recife')
@@ -90,12 +92,12 @@ console.log('Hora: ' + defaultTimeZoneHour)
 console.log('Tipo do dado Hora é um: ' + typeof defaultTimeZoneHour)
 console.log('----------------------------')
 
-exports.handler = async event => {
-    try {
-        await bot.handleUpdate(JSON.parse(event.body))
-        return { statusCode: 200, body: "" }
-    } catch (e) {
-        console.error("error in handler:", e)
-        return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
-    }
-}
+// exports.handler = async event => {
+//     try {
+//         await bot.handleUpdate(JSON.parse(event.body))
+//         return { statusCode: 200, body: "" }
+//     } catch (e) {
+//         console.error("error in handler:", e)
+//         return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+//     }
+// }
